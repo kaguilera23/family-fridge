55ac429267bc00a728ef6c952ce1f3fd19e6a598
unknown <aguilera.k02@gmail.com>
2023-03-07 20:23:13 -0500

    add page photo

---
+++ src/components/Main.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Header from "./Header";
+import AboutMe from "./pages/AboutMe";
+import Portfolio from "./pages/Portfolio";
+import ContactMe from "./pages/ContactMe";
+import Resume from "./pages/Resume";
+import Footer from "./Footer"
+
+export default function MainPage() {
+  const [currentPage, setCurrentPage] = useState("AboutMe");
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "AboutMe":
+        return <AboutMe />;
+      case "Portfolio":
+        return <Portfolio />;
+      case "ContactMe": 
+        return <ContactMe />;
+      case "Resume":
+        return <Resume />;
+      default: 
+        return <AboutMe />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer />
+    </div>
+  );
+}
