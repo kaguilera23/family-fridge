68a45b425764000dd941762fad2a8ed499b5d1cd
unknown <aguilera.k02@gmail.com>
2023-03-22 18:08:02 -0400

    added icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,19 @@
 import "./styles/Footer.css"
+import { GithubOutlined, LinkedinOutlined, InstagramOutlined } from '@ant-design/icons';
 
-const styles = {
-    links: {
-        textDecoration: "none",
-        color: "white"
-    }
-}
 
 function Footer() {
     return (
         <div className="footer" id = "main-footer">
-            <p id="links">
-                <a href="https://github.com/kaguilera23" style={styles.links}>GitHub Profile</a>
-                <a href="https://www.linkedin.com/in/kaylanaguilera/" style={styles.links}>LinkedIn</a>
-                <a href="https://open.spotify.com/user/5tqhcf6f1lzhppgrdjrvoduqk?si=bd6ad24d1cbf420f" style={styles.links}>Spotify</a>
-            </p>            
+
+            <div id="links">
+                <GithubOutlined onClick={() => window.location.href="https://github.com/kaguilera23"} style={{fontSize: 50}}></GithubOutlined>
+                <LinkedinOutlined onClick={() => window.location.href="https://www.linkedin.com/in/kaylanaguilera/"} style={{fontSize: 50}}></LinkedinOutlined>
+                <InstagramOutlined onClick={() => window.location.href="https://www.instagram.com/lil_llama23/?hl=en"} style={{fontSize: 50}}></InstagramOutlined>
+            </div>  
+            
         </div>
+
     )
 }
 
