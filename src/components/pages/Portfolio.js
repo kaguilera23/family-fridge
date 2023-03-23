ae31b36464eb983ad5ee55b13a4521bdb171772d
unknown <aguilera.k02@gmail.com>
2023-03-23 15:35:36 -0400

    fixed portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,74 +1,44 @@
 import "../styles/Portfolio.css"
+import React from 'react';
+import { Card, Row } from 'antd';
 
-const styles = {
-    cardClass: {
-        width: "18rem"
-    }
-}
+const { Meta } = Card;
 
 function Portfolio() {
     return (
-        <div id="cards-section">
-            <div className="card" style={styles.card} id="project-cards">
-                <div className="project-picture "id="weather"></div>
-                <div className="card-body">
-                    <h5 className="card-title">Weather Dashboard</h5>
-                    <p className="card-text">See the current and future weather forecasts for your area</p>
-                    <a className="btn btn-primary" href="https://github.com/kaguilera23/sunshine-rain-dashboard" id="nav-buttons">GitHub Repository</a>
-                    <a className="btn btn-primary" href="https://kaguilera23.github.io/sunshine-rain-dashboard/" id="nav-buttons">Live Link</a>
-                </div>
-            </div>
+        <Row id="cards-section">
+            <Card
+                hoverable style={{ width: 300 }} cover={<img alt="caffiends" src="https://i.imgur.com/BQpitRc.png" />}>
+                <Meta title="Caffiends" description="A local coffee shop's dream site! Users can login and keep track of rewards points and order history, but you can also order without an account." />
+                <section id="proj-links">
+                    <a href="https://caffiends.herokuapp.com/" target="_blank" rel="noreferrer" className="a-links">Live Website</a>
+                    <a href="https://github.com/kaguilera23/caffiends" target="_blank" rel="noreferrer" className="a-links">GitHub Repository</a>                    
+                </section>
+            </Card>     
 
-            <div className="card" style={styles.card} id="project-cards">
-                <div className="project-picture" id="medi"></div>
-                <div className="card-body">
-                    <h5 className="card-title">My Medi</h5>
-                    <p className="card-text">Keep track of important medications and medical information</p>
-                    <a className="btn btn-primary" href="https://github.com/kaguilera23/my-medi" id="nav-buttons">GitHub Repository</a>
-                    <a className="btn btn-primary" href="https://my-medi.herokuapp.com/" id="nav-buttons">Live Link</a>
-                </div>
-            </div>
+            <Card
+                hoverable
+                style={{ width: 300 }}
+                cover={<img alt="medi" src="https://i.imgur.com/GQfF3uQ.png" />}
+            >
+                <Meta title="My Medi" description="A website that helps users keep track of the medications they're on as well as other important medical information such as blood type, emergency contact, allergies, etc." />
+                <section id="proj-links">
+                    <a href="https://github.com/kaguilera23/caffiends" target="_blank" rel="noreferrer" className="a-links">GitHub Repository</a>                    
+                </section>
+            </Card>    
 
-            <div className="card" style={styles.card} id="project-cards">
-                <div className="project-picture" id="password"></div>
-                <div className="card-body">
-                    <h5 className="card-title">Password Generator</h5>
-                    <p className="card-text">Create a strong, unhackable password quickly</p>
-                    <a className="btn btn-primary" href="https://github.com/kaguilera23/password-generator" id="nav-buttons">GitHub Repository</a>
-                    <a className="btn btn-primary" href="https://kaguilera23.github.io/password-generator/" id="nav-buttons">Live Link</a>
-                </div>
-            </div>
-
-            <div className="card" style={styles.card} id="project-cards">
-                <div className="project-picture" id="binge"></div>
-                <div className="card-body">
-                    <h5 className="card-title">Binge Show Calculator</h5>
-                    <p className="card-text">Calculate how long it will take you to watch your favorite show, even with your busy, hectic schedule</p>
-                    <a className="btn btn-primary" href="https://github.com/kaguilera23/Binge-Show-Calculator-p01" id="nav-buttons">GitHub Repository</a>
-                    <a className="btn btn-primary" href="https://camh915.github.io/Binge-Show-Calculator-p01/" id="nav-buttons">Live Link</a>
-                </div>
-            </div>
-
-            <div className="card" style={styles.card} id="project-cards">
-                <div className="project-picture" id="quiz"></div>
-                <div className="card-body">
-                    <h5 className="card-title">Coding Quiz</h5>
-                    <p className="card-text">See how much you know about coding with this quick, five-question quiz</p>
-                    <a className="btn btn-primary" href="https://github.com/kaguilera23/code-quiz" id="nav-buttons">GitHub Repository</a>
-                    <a className="btn btn-primary" href="https://kaguilera23.github.io/code-quiz/" id="nav-buttons">Live Link</a>
-                </div>
-            </div>
-
-            <div className="card" style={styles.card} id="project-cards">
-                <div className="project-picture" id="schedule"></div>
-                <div className="card-body">
-                    <h5 className="card-title">Word Day Scheduler</h5>
-                    <p className="card-text">Keep track of all the things you have to do in a day with this simple, easy-to-use application</p>
-                    <a className="btn btn-primary" href="https://github.com/kaguilera23/busy-bee-organizer" id="nav-buttons">GitHub Repository</a>
-                    <a className="btn btn-primary" href="https://kaguilera23.github.io/busy-bee-organizer/" id="nav-buttons">Live Link</a>
-                </div>
-            </div>                        
-        </div>
+            <Card
+                hoverable
+                style={{ width: 300 }}
+                cover={<img alt="binge" src="https://i.imgur.com/24ekWQF.png" />}
+            >
+                <Meta title="Binge Show Calculator" description="Unlike other show calculators, this one takes into account the hours in a day you're busy with other things such as work and adds it to the total run time of a show, giving you a realistic timeframe to watch your favorite shows. " />
+                <section id="proj-links">
+                    <a href="https://camh915.github.io/Binge-Show-Calculator-p01/" target="_blank" rel="noreferrer" className="a-links">Live Website</a>
+                    <a href="https://github.com/kaguilera23/Binge-Show-Calculator-p01" target="_blank" rel="noreferrer" className="a-links">GitHub Repository</a>                    
+                </section>
+            </Card>         
+        </Row>
     )
 };
 
